d1adb17ecff75867e09832b2315b10d0dc7e10e5
Drake Johnson <237334005+drakepwj@users.noreply.github.com>
2025-12-05 15:20:30 -0700

    removed comment from backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "git-vault-360166fb"   # your actual bucket from Part 3
+    bucket = "git-vault-360166fb" 
     key    = "terraform/state.tfstate"
     region = "us-west-2"
   }
